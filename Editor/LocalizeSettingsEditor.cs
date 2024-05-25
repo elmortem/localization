9c6bd5e7b1159493f95f295e8eba1cfe63b9aee8
Makar Osokin <elmortem@gmail.com>
2024-05-25 14:45:54 +0300

    Current locales list in settings Version 1.0.1

--- Editor/LocalizeSettingsEditor.cs
+++ Editor/LocalizeSettingsEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,8 +8,16 @@ namespace Localization.Editor
 	[CustomEditor(typeof(LocalizeSettings))]
 	public class LocalizeSettingsEditor : UnityEditor.Editor
 	{
+		private GUIStyle _headerStyle;
+
 		public override void OnInspectorGUI()
 		{
+			if (_headerStyle == null)
+			{
+				_headerStyle = new GUIStyle(GUI.skin.label);
+				_headerStyle.fontStyle = FontStyle.Bold;
+			}
+			
 			var settings = (LocalizeSettings)target;
 			
 			if (GUILayout.Button("Load All from Google Sheet"))
@@ -22,6 +31,17 @@ namespace Localization.Editor
 			}
 			
 			base.OnInspectorGUI();
+			
+			GUILayout.Space(10f);
+			GUILayout.Label("Current Locales", _headerStyle);
+			foreach (var locale in settings.Locales)
+			{
+				if (GUILayout.Button($"{locale.Language}"))
+				{
+					var localeAsset = locale.Load();
+					EditorGUIUtility.PingObject(localeAsset);
+				}
+			}
 		}
 	}
 }
