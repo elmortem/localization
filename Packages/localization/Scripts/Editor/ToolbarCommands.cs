4c9e936516b800ebc87ad516621e180478ee8f97
Andrey Boronnikov <boronnikov_a_i@mail.ru>
2024-10-25 12:38:02 +0300

    Added toolbar fast command Load all from Google Sheet

---
+++ Packages/localization/Scripts/Editor/ToolbarCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace Localization.Editor
+{
+	public static class ToolbarCommands
+	{
+		[MenuItem("Tools/Localization/Load all from Google Sheet")]
+		public static void LoadAllFromGoogleSheet()
+		{
+			var settings = LocalizeSettings.MakeInstance();
+			var localeAssets = new List<LocaleAsset>();
+			foreach (var locale in settings.Locales)
+			{
+				localeAssets.Add(locale.Load());
+			}
+			EditorLocalizeSystem.LoadFromGoogleSheet(localeAssets);
+		}
+	}
+}
